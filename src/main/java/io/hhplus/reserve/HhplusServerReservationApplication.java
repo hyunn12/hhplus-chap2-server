0990562f91d0ac18d74bfafa080693212f47f276
hyunn12 <hyunn964@gmail.com>
2024-10-10 00:51:52 +0900

    feat: 프로젝트 초기 구성 [#6]

---
+++ src/main/java/io/hhplus/reserve/HhplusServerReservationApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusServerReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusServerReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/hhplus/reserve/HhplusServerReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusServerReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
